baed64dda894d277ba52b1d77afc273e8313c67c
josephdpandaravalappil <josephdpandaravalappil@gmail.com>
2017-01-28 19:39:05 +0530

    Added static root in settings.

--- quiz/settings.py
+++ quiz/settings.py
@@ -108,7 +108,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 LOGIN_URL = '/users/login'
-STATIC_ROOT = os.path.join(BASE_DIR, '/quiz/static')
+STATIC_ROOT = BASE_DIR + '/quiz/static'
